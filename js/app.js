dcd584963eab76a56a300fb8b7603a8ff0522885
laugeeme <registro.laura@gmail.com>
2020-05-21 11:50:13 +0200

    add event listeners to all inputs

--- js/app.js
+++ js/app.js
@@ -6,20 +6,19 @@ const subject = document.getElementById('asunto');
 const msj = document.getElementById('mensaje');
 const sendBtn = document.getElementById('enviar');
 
-
-
-
-
-
 //disable sendBtn at app started
-function startApp(){
-    sendBtn.disabled = true;
+function startApp() {
+  sendBtn.disabled = true;
 }
 
+//verify if there is some text
+function validateInput() {
+    //validates the length of the text and that the field is not empty
 
-
-
-
+}
 
 //event
 document.addEventListener('DOMContentLoaded', startApp);
+email.addEventListener('blur', validateInput);
+subject.addEventListener('blur', validateInput);
+msj.addEventListener('blur', validateInput);
